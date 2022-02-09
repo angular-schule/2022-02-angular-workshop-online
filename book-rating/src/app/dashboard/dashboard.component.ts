dcd3abdad968ba8cdd0ea945652413b71d7a77de
Ferdinand Malcher <ferdinand@malcher.media>
2022-02-09 12:27:21 +0100

    Dashboard mit Büchern

---
+++ book-rating/src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { Book } from '../shared/book';
+
+@Component({
+  selector: 'br-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss']
+})
+export class DashboardComponent implements OnInit {
+
+  books: Book[] = [];
+
+  constructor() {
+    this.books = [
+      {
+        isbn: '123',
+        title: 'Angular',
+        description: 'Grundlagen und mehr',
+        price: 36.9,
+        rating: 5
+      },
+      {
+        isbn: '456',
+        title: 'Vue.js',
+        description: 'Das grüne Framework',
+        price: 32.9,
+        rating: 3
+      }
+    ];
+  }
+
+  ngOnInit(): void {
+  }
+
+}
