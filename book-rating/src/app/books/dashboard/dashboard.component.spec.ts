c2aa20a531f9c4bbd910c048f77494b31002630f
Ferdinand Malcher <ferdinand@malcher.media>
2022-02-11 11:16:26 +0100

    Test Dashboard

--- book-rating/src/app/books/dashboard/dashboard.component.spec.ts
+++ book-rating/src/app/books/dashboard/dashboard.component.spec.ts
@@ -1,15 +1,31 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { BookComponent } from '../book/book.component';
+import { Book } from '../shared/book';
+import { BookRatingService } from '../shared/book-rating.service';
 
 import { DashboardComponent } from './dashboard.component';
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
   let fixture: ComponentFixture<DashboardComponent>;
+  let book: Book;
 
   beforeEach(async () => {
+    const ratingMock: Partial<BookRatingService> = {
+      rateUp: (b: Book) => b,
+      rateDown: (b: Book) => b,
+    };
+
+    book = { isbn: '12345' } as Book;
+
     await TestBed.configureTestingModule({
-      declarations: [ DashboardComponent ]
+      declarations: [ DashboardComponent ],
+      providers: [
+        // BRS ersetzen: immer wenn jemand BRS anfordert, wird stattdessen ratingMock ausgeliefert
+        {
+          provide: BookRatingService,
+          useValue: ratingMock
+        }
+      ]
     })
     .compileComponents();
   });
@@ -17,10 +33,23 @@ describe('DashboardComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(DashboardComponent);
     component = fixture.componentInstance;
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call the service for doRateUp()', () => {
+    // Arrange
+    const service = TestBed.inject(BookRatingService); // das ist eigentlich der ratingMock!
+    spyOn(service, 'rateUp').and.callThrough();
+
+    // Act
+    component.doRateUp(book);
+
+    // Assert
+    expect(service.rateUp).toHaveBeenCalledOnceWith(book);
   });
 });
