5d4e3b6bb9abd42f8d0fb8295991c6c25b18bf49
Ferdinand Malcher <ferdinand@malcher.media>
2022-02-11 10:11:19 +0100

    ChangeDetection OnPush

--- book-rating/src/app/books/book/book.component.ts
+++ book-rating/src/app/books/book/book.component.ts
@@ -1,10 +1,11 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Book } from '../shared/book';
 
 @Component({
   selector: 'br-book',
   templateUrl: './book.component.html',
-  styleUrls: ['./book.component.scss']
+  styleUrls: ['./book.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class BookComponent implements OnInit {
 
@@ -25,5 +26,9 @@ export class BookComponent implements OnInit {
 
   doRateDown() {
     this.book && this.rateDown.emit(this.book);
+  }
+
+  log() {
+    console.log('CD', Date.now());
   }
 }
