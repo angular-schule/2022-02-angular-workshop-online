30bb00e3ae260985df6c0a87b0e4eed1874f1b2f
Ferdinand Malcher <ferdinand@malcher.media>
2022-02-16 10:38:08 +0100

    RxJS: Multicast

--- rxjs-playground/src/app/exercises/multicast/multicast.component.ts
+++ rxjs-playground/src/app/exercises/multicast/multicast.component.ts
@@ -13,12 +13,17 @@ export class MulticastComponent {
   listeners: string[] = [];
   logStream$ = new ReplaySubject<string>();
 
-  measureValues$: Observable<number>; // später: Subject<number>;
+  measureValues$: Subject<number>;
 
   constructor(private mvs: MeasureValuesService, private es: ExerciseService) {
-    /**************!!**************/
-    this.measureValues$ = this.mvs.getValues();
-    /**************!!**************/
+    // this.measureValues$ = this.mvs.getValues().pipe(share());
+    // this.measureValues$ = this.mvs.getValues().pipe(shareReplay(1));
+
+    this.measureValues$ = new ReplaySubject(5);
+    this.mvs.getValues().subscribe(this.measureValues$);
+
+    // nur Observable, kein next/error/complete
+    const obs$ = this.measureValues$.asObservable();
 
   }
 
